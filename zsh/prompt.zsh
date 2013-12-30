3cd706d2b8adf370133b0a698bc120ebd2ad77da
Coty Rosenblath <coty@pobox.com>
2013-12-30 15:45:21 -0500

    Add support for RVM in prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -50,9 +50,12 @@ need_push () {
 rb_prompt(){
   if (( $+commands[rbenv] ))
   then
-	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-	else
-	  echo ""
+    echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
+  elif (( $+commands[rvm] ))
+  then
+    echo "%{$fg_bold[yellow]%}$(rvm current | awk '{print $1}')%{$reset_color%}"
+  else
+    echo ""
   fi
 }
 
